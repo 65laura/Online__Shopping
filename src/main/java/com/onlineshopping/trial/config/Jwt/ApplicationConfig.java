35c873d0bdbf813e0d3184df8bfd5fe55a40785c
LENOVO <ishimwelauraceline@gmail.com>
2024-01-23 19:54:15 +0200

    security

---
+++ src/main/java/com/onlineshopping/trial/config/Jwt/ApplicationConfig.java
@@ -0,0 +1,26 @@
+package com.onlineshopping.trial.config.Jwt;
+
+import com.onlineshopping.trial.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private UserRepository userRepository;
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username)
+                        .orElseThrow(() ->new UsernameNotFoundException("user not found"));
+            }
+        };
+    }
+
+}
--- src/main/java/com/onlineshopping/trial/config/Jwt/AuthenticationFilter.java
+++ src/main/java/com/onlineshopping/trial/config/Jwt/AuthenticationFilter.java
@@ -1,18 +1,21 @@
 package com.onlineshopping.trial.config.Jwt;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.sql.results.graph.entity.AbstractEntityResultGraphNode;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class AuthenticationFilter extends OncePerRequestFilter {
+    private  JwtService jwtService;
+    private UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(
          @NonNull HttpServletRequest request,
@@ -21,11 +24,18 @@ public class AuthenticationFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
         final String authHeader= request.getHeader("Authorization");
         final String jwt ;
+        final String userEmail;
         if (authHeader== null ||!authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request,response);
             return ;
         }
         jwt = authHeader.substring(7);
+        userEmail= jwtService.extractUsername(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication()== null){
+            UserDetails userDetails= this.userDetailsService.loadUserByUsername(userEmail);
+        //if(jwtService.isTokenValid(jwt,userDetails))
+
+        }
 
 
     }
