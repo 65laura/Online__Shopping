36aa0f961380ae36898ab958d999bee6ab1b1d8a
LENOVO <ishimwelauraceline@gmail.com>
2024-01-24 15:17:41 +0200

    security

---
+++ src/main/java/com/onlineshopping/trial/config/Jwt/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.onlineshopping.trial.config.Jwt;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+      //  http
+               // .csrf()
+               // .disable()
+               // .authorizeHttpRequests()
+               // .requestMatchers()
+               // .permitAll()
+              //  .anyRequest()
+              //  .authenticated()
+              //  .and()
+              //  .sessionManagement()
+               // .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+              //  .authenticationProvider
+
+                return http.build();
+    }
+}
