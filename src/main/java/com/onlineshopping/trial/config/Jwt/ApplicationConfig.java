fcf50f928237dc9bb6c279ee81dac43d1de3cae3
LENOVO <ishimwelauraceline@gmail.com>
2024-01-25 15:09:32 +0200

    security

--- src/main/java/com/onlineshopping/trial/config/Jwt/ApplicationConfig.java
+++ src/main/java/com/onlineshopping/trial/config/Jwt/ApplicationConfig.java
@@ -1,12 +1,17 @@
 package com.onlineshopping.trial.config.Jwt;
-
 import com.onlineshopping.trial.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,7 +25,22 @@ public class ApplicationConfig {
                 return userRepository.findByEmail(username)
                         .orElseThrow(() ->new UsernameNotFoundException("user not found"));
             }
+            @Bean
+            public AuthenticationProvider authenticationProvider(){
+                DaoAuthenticationProvider authenticationProvider= new DaoAuthenticationProvider();
+                authenticationProvider.setUserDetailsService(userDetailsService());
+                authenticationProvider.setPasswordEncoder(passwordEncoder());
+                return authenticationProvider;
+            }
+            @Bean
+            public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+               return configuration.getAuthenticationManager();
+            }
         };
+    }
+@Bean
+    public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/onlineshopping/trial/config/Jwt/SecurityConfiguration.java
+++ src/main/java/com/onlineshopping/trial/config/Jwt/SecurityConfiguration.java
@@ -2,29 +2,36 @@ package com.onlineshopping.trial.config.Jwt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+   // private JwtAuthentication jwtAuthFilter;
+    private AuthenticationProvider authenticationProvider;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
-      //  http
-               // .csrf()
-               // .disable()
-               // .authorizeHttpRequests()
-               // .requestMatchers()
-               // .permitAll()
-              //  .anyRequest()
-              //  .authenticated()
-              //  .and()
-              //  .sessionManagement()
-               // .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-              //  .authenticationProvider
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/auth").permitAll()
+                        .anyRequest().authenticated());
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
-                return http.build();
-    }
-}
+               return http.build();
+}}
