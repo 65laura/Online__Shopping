31e08957bff8fb93649f8076ddc1c4e2289943ac
65laura <ishimwelauraceline@gmail.com>
2024-10-22 20:14:21 +0200

    adjustments

--- src/main/java/com/onlineshopping/trial/controller/AttachmentController.java
+++ src/main/java/com/onlineshopping/trial/controller/AttachmentController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("attachment")
+@RequestMapping("")
 @RequiredArgsConstructor
 public class AttachmentController {
     private final IAttachmentService attachmentService;
